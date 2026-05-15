c8e16ae29b99a4302a1ff18529ac6e8972fe83c1
yamunaaravikumar <yamunaaravikumar@gmail.com>
2026-05-15 20:02:10 +0530

    added railway mysql

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
+DB_PORT=os.getenv('DB_PORT')
 
 def get_db_connection():
     try:
@@ -21,7 +22,8 @@ def get_db_connection():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_NAME,
+            port=DB_PORT
         )
         return conn
     except mysql.connector.Error as err:
